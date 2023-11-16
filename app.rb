00dc0c2a49db9fb63ddb770e692f278e69a93bca
cruz-welandi <cruzwelandi@gmail.com>
2023-11-16 18:40:10 +0100

    correction

--- app.rb
+++ app.rb
@@ -1,30 +1,44 @@
 require 'sinatra'
+require 'json'
 require './my_user_model'
 
+users = User.new('db.sql')
 enable :sessions
 
-get '/' do
-    @user = {
-        firstname: 'John',
-        lastname: 'Doe',
-        age: 30,
-        email: 'john@example.com'
-      }
-    
-      File.read(File.join('views', 'index.html')).gsub(/\{\{(\w+)\}\}/) { @user[$1.to_sym] }
+user_info1 = {firstname: 'hance', lastname: 'hance', age: 30, password: '2145', email: 'hance@example.com'}
+user_id1 = users.create(user_info1)
+
+user_info2 = {firstname: 'Jane', lastname: 'Doe', age: 28, password: 'password456', email: 'jane.doe@example.com'}
+user_id2 = users.create(user_info2)
+
+get '/users' do
+  content_type :json
+  all_users = users.all
+  all_users.each { |user| user.delete(:password) } # Supprime le mot de passe de chaque utilisateur
+  all_users.to_json
 end
 
-user_db = User.new('db.sql')
+post '/users' do
+    content_type :json
+    user_info = JSON.parse(request.body.read)
+    user_id = users.create(user_info)
+    user = users.find(user_id)
+    user.delete(:password) # Supprime le mot de passe avant de renvoyer l'utilisateur
+    user.to_json
+end
 
+post '/sign_in' do
+  content_type :json
+  user_info = JSON.parse(request.body.read)
+  user = users.find_by_email(user_info["email"])
+  if user && user[:password] == user_info["password"]
+    session[:user_id] = user[:id] # Crée une session avec l'identifiant de l'utilisateur
+    user.delete(:password) # Supprime le mot de passe avant de renvoyer l'utilisateur
+    user.to_json
+  else
+    status 401 # Code d'erreur pour une authentification non réussie
+    {error: "Invalid email or password"}.to_json
+  end
+end
 
-# Create a new user
-new_user_id = user_db.create({
-  firstname: 'John',
-  lastname: 'Doe',
-  age: 30,
-  password: 'securepassword',
-  email: 'john@example.com'
-})
-
-
-set :port, 8080  # Port sur lequel le serveur écoutera
+set :port, 8080  # Port sur lequel le serveur écoutera   require 'sqlite3'
--- my_user_model.rb
+++ my_user_model.rb
@@ -7,50 +7,9 @@ class User
         create_table
     end
 
-    def create(user_info)
-        sql = 'INSERT INTO users (firstname, lastname, age, password, email) VALUES (?, ?, ?, ?, ?)'
-        @db.execute(sql, user_info[:firstname], user_info[:lastname], user_info[:age], user_info[:password], user_info[:email])
-        
-        last_insert_row_id = @db.last_insert_row_id
-        return last_insert_row_id if last_insert_row_id > 0
-        return nil
-    end
-
-    def find(user_id)
-        sql = 'SELECT * FROM users WHERE id = ?'
-        user = @db.get_first_row(sql, user_id)
-        user ? user_hash(user) : nil
-    end
-
-    def all
-        sql = 'SELECT * FROM users'
-        users = @db.execute(sql)
-        users.map { |user| user_hash(user) }
-    end
-
-    def update(user_id, attribute, value)
-        valid_attributes = %w[first_name last_name age password email]
-        unless valid_attributes.include?(attribute)
-          return "Invalid attribute"
-        end
-
-        sql = "UPDATE users SET #{attribute} = ? WHERE id = ?"
-        @db.execute(sql, value, user_id)
-
-        user = find(user_id)
-        user ? user : nil
-    end
-
-    def destroy(user_id)
-        sql = 'DELETE FROM users WHERE id = ?'
-        @db.execute(sql, user_id)
-    end
-
-    private
-
     def create_table
-        @db.execute <<~SQL
-          CREATE TABLE IF NOT EXISTS users (
+        @db.execute <<-SQL
+          CREATE TABLE IF NOT EXISTS Users(
             id INTEGER PRIMARY KEY,
             firstname TEXT,
             lastname TEXT,
@@ -61,15 +20,35 @@ class User
         SQL
     end
 
-    def user_hash(user)
-        {
-          id: user[0],
-          firstname: user[1],
-          lastname: user[2],
-          age: user[3],
-          password: user[4],
-          email: user[5]
-        }
+    def create_user(firstname, lastname, age, password)
+        @db.execute("INSERT INTO Users (firstname, lastname, age, password) VALUES (?, ?, ?, ?)", firstname, lastname, age, password)
+    end
+
+    def create(user_info)
+        @db.execute("INSERT INTO Users (firstname, lastname, age, password, email) VALUES (?, ?, ?, ?, ?)", user_info[:firstname], user_info[:lastname], user_info[:age], user_info[:password], user_info[:email])
+        return @db.last_insert_row_id
+    end
+
+    def find(user_id)
+        user = @db.get_first_row("SELECT * FROM Users WHERE id = ?", user_id)
+        return nil if user.nil?
+        {id: user[0], firstname: user[1], lastname: user[2], age: user[3], password: user[4], email: user[5]}
+    end
+
+    def all
+        users = @db.execute("SELECT * FROM Users")
+        users.map do |user|
+          {id: user[0], firstname: user[1], lastname: user[2], age: user[3], password: user[4], email: user[5]}
+        end
+    end
+
+    def update(user_id, attribute, value)
+        @db.execute("UPDATE Users SET #{attribute} = ? WHERE id = ?", value, user_id)
+        find(user_id)
+    end
+
+    def destroy(user_id)
+        @db.execute("DELETE FROM Users WHERE id = ?", user_id)
     end
 
 end
