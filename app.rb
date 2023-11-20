e47ca4c0c78ace16fe33c18d1973ffb27f59e6f7
cruz-welandi <cruzwelandi@gmail.com>
2023-11-20 03:54:32 +0100

    version correct

--- app.rb
+++ app.rb
@@ -2,31 +2,76 @@ require 'sinatra'
 require 'json'
 require './my_user_model'
 
-users = User.new('db.sql')
 enable :sessions
 
-user_info1 = {firstname: 'hance', lastname: 'hance', age: 30, password: '2145', email: 'hance@example.com'}
-user_id1 = users.create(user_info1)
+user_info1 = {id: 1, firstname: 'hance', lastname: 'hance', age: 30, password: '2145', email: 'hance@example.com'}
+user_id1 = User.create(user_info1)
 
-user_info2 = {firstname: 'Jane', lastname: 'Doe', age: 28, password: 'password456', email: 'jane.doe@example.com'}
-user_id2 = users.create(user_info2)
+#user_info2 = {firstname: 'Jane', lastname: 'Doe', age: 28, password: 'password456', email: 'jane.doe@example.com'}
+#user_id2 = User.create(user_info2)
+
+get '/' do
+  send_file 'views/index.html'
+end
 
 get '/users' do
   content_type :json
-  all_users = users.all
-  all_users.each { |user| user.delete(:password) } # Supprime le mot de passe de chaque utilisateur
-  all_users.to_json
+  all_users = User.all
+  users_without_passwords = all_users.map do |user|
+    {
+      id: user.id,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      age: user.age,
+      email: user.email
+    }
+  end
+  users_without_passwords.to_json
 end
 
 post '/users' do
-    content_type :json
-    user_info = JSON.parse(request.body.read)
-    user_id = users.create(user_info)
-    user = users.find(user_id)
-    user.delete(:password) # Supprime le mot de passe avant de renvoyer l'utilisateur
-    user.to_json
+
+
+  # Réinitialiser le pointeur de lecture du corps de la requête
+  request.body.rewind
+
+  # Analyser le corps de la requête JSON
+  begin
+    request_payload = Rack::Utils.parse_nested_query(request.body.read)
+  rescue JSON::ParserError => e
+    # Retourner une réponse 400 Bad Request si le format JSON est invalide
+    halt 400, { error: 'Format JSON invalide.' }.to_json
+  end
+
+  # Créer un nouvel utilisateur avec les données fournies
+  new_user = User.create(
+    firstname: request_payload['firstname'],
+    lastname: request_payload['lastname'],
+    age: request_payload['age'],
+    password: request_payload['password'],
+    email: request_payload['email']
+  )
+
+  # Vérifier si la création de l'utilisateur a réussi
+  if new_user
+    # Construire la réponse avec les informations de l'utilisateur (sans le mot de passe)
+    response_data = {
+      id: new_user.id,
+      firstname: new_user.firstname,
+      lastname: new_user.lastname,
+      age: new_user.age,
+      email: new_user.email
+    }
+
+    # Renvoyer la réponse en format JSON
+    response_data.to_json
+  else
+    # En cas d'échec, retourner une réponse 500 Internal Server Error
+    halt 500, { error: 'Erreur lors de la création de l\'utilisateur.' }.to_json
+  end
 end
 
+=begin
 post '/sign_in' do
   content_type :json
   user_info = JSON.parse(request.body.read)
@@ -40,5 +85,70 @@ post '/sign_in' do
     {error: "Invalid email or password"}.to_json
   end
 end
+
+put '/users' do
+  content_type :json
+
+  # Vérifiez si l'utilisateur est connecté
+  if session[:user_id]
+    user_info = JSON.parse(request.body.read)
+    user = users.find(session[:user_id])
+
+    if user
+      # Mettez à jour le mot de passe de l'utilisateur
+      user[:password] = user_info["new_password"]
+
+      # Supprimez le mot de passe avant de renvoyer l'utilisateur
+      user.delete(:password)
+
+      # Renvoyez l'utilisateur mis à jour
+      user.to_json
+    else
+      status 404
+      {error: "User not found"}.to_json
+    end
+  else
+    status 401
+    {error: "You must be logged in to perform this action"}.to_json
+  end
+end
+
+
+delete '/sign_out' do
+  # Vérifiez si l'utilisateur est connecté
+  if session[:user_id]
+    # Déconnectez l'utilisateur
+    session.clear
+    # Renvoyez un code de statut HTTP 204 (No Content)
+    status 204
+  else
+    status 401
+    {error: "Vous devez être connecté pour effectuer cette action"}.to_json
+  end
+end
+
+
+delete '/users' do
+  # Vérifiez si l'utilisateur est connecté
+  if session[:user_id]
+    # Trouvez l'utilisateur actuel
+    user = users.find(session[:user_id])
+    if user
+      # Déconnectez l'utilisateur
+      session.clear
+      # Supprimez l'utilisateur
+      users.delete(user)
+      # Renvoyez un code de statut HTTP 204 (No Content)
+      status 204
+    else
+      status 404
+      {error: "Utilisateur non trouvé"}.to_json
+    end
+  else
+    status 401
+    {error: "Vous devez être connecté pour effectuer cette action"}.to_json
+  end
+end
+=end
 
 set :port, 8080  # Port sur lequel le serveur écoutera   require 'sqlite3'
--- my_user_model.rb
+++ my_user_model.rb
@@ -1,54 +1,65 @@
 require 'sqlite3'
 
 class User
+  attr_accessor :id, :firstname, :lastname, :age, :password, :email
 
-    def initialize(bd)
-        @db = SQLite3::Database.new('db.sql')
-        create_table
-    end
+  def initialize(id:, firstname:, lastname:, age:, password:, email:)
+    @id = id
+    @firstname = firstname
+    @lastname = lastname
+    @age = age
+    @password = password
+    @email = email
+  end
 
-    def create_table
-        @db.execute <<-SQL
-          CREATE TABLE IF NOT EXISTS Users(
-            id INTEGER PRIMARY KEY,
-            firstname TEXT,
-            lastname TEXT,
-            age INTEGER,
-            password TEXT,
-            email TEXT
-          );
-        SQL
-    end
+  def self.create(user_info)
+    user_info[:id] ||= nil
+    user = new(**user_info)
 
-    def create_user(firstname, lastname, age, password)
-        @db.execute("INSERT INTO Users (firstname, lastname, age, password) VALUES (?, ?, ?, ?)", firstname, lastname, age, password)
-    end
+    db = SQLite3::Database.new 'db.sql'
+    db.execute <<-SQL
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        firstname TEXT,
+        lastname TEXT,
+        age INTEGER,
+        password TEXT,
+        email TEXT
+      );
+    SQL
+    db.execute "INSERT INTO users (firstname, lastname, age, password, email) VALUES (?, ?, ?, ?, ?)", [user.firstname, user.lastname, user.age, user.password, user.email]
 
-    def create(user_info)
-        @db.execute("INSERT INTO Users (firstname, lastname, age, password, email) VALUES (?, ?, ?, ?, ?)", user_info[:firstname], user_info[:lastname], user_info[:age], user_info[:password], user_info[:email])
-        return @db.last_insert_row_id
-    end
+    # Récupérez l'ID du dernier enregistrement inséré
+    user.id = db.last_insert_row_id
+    user
+  end
 
-    def find(user_id)
-        user = @db.get_first_row("SELECT * FROM Users WHERE id = ?", user_id)
-        return nil if user.nil?
-        {id: user[0], firstname: user[1], lastname: user[2], age: user[3], password: user[4], email: user[5]}
+  def self.find(id)
+    db = SQLite3::Database.new 'db.sql'
+    result = db.execute "SELECT * FROM users WHERE id = ?", [id]
+    if result.empty?
+      nil
+    else
+      new(id: result[0][0], firstname: result[0][1], lastname: result[0][2], age: result[0][3], password: result[0][4], email: result[0][5])
     end
+  end
 
-    def all
-        users = @db.execute("SELECT * FROM Users")
-        users.map do |user|
-          {id: user[0], firstname: user[1], lastname: user[2], age: user[3], password: user[4], email: user[5]}
-        end
+  def self.all
+    db = SQLite3::Database.new 'db.sql'
+    results = db.execute "SELECT * FROM users"
+    results.map do |row|
+      new(id: row[0], firstname: row[1], lastname: row[2], age: row[3], password: row[4], email: row[5])
     end
+  end
 
-    def update(user_id, attribute, value)
-        @db.execute("UPDATE Users SET #{attribute} = ? WHERE id = ?", value, user_id)
-        find(user_id)
-    end
+def self.update(id, attribute, value)
+    db = SQLite3::Database.new 'db.sql'
+    db.execute "UPDATE users SET #{attribute} = ? WHERE id = ?", [value, id]
+end
 
-    def destroy(user_id)
-        @db.execute("DELETE FROM Users WHERE id = ?", user_id)
-    end
+def self.destroy(id)
+    db = SQLite3::Database.new 'db.sql'
+    db.execute "DELETE FROM users WHERE id = ?", [id]
+end
 
 end
